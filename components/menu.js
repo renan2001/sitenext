a8d8018efab4142f150eabf8129939eed70f14f0
renan2001 <renancastro2216@gmail.com>
2021-08-30 14:43:56 -0300

    ewer

--- components/menu.js
+++ components/menu.js
@@ -1,5 +1,12 @@
 function MENU(){
-    return <div>Aqui você vai fazer o código do seu menu</div>
+    return <div>Aqui você vai fazer o código do seu menu
+        <br>
+        </br>
+        <ul>
+            <li>primeiro</li>
+            <li>segundo </li>
+        </ul>
+    </div>
 }
 
 export default MENU
