ffd6f8cc61b3c078c22b9086b0dc388ffd8e1e63
renan2001 <renancastro2216@gmail.com>
2021-08-30 16:46:53 -0300

    .

--- components/menu.js
+++ components/menu.js
@@ -1,12 +1,25 @@
+
+
+export default MENU
+
+
+import Link from 'next/link'
+
 function MENU(){
-    return <div>Aqui você vai fazer o código do seu menu
-        <br>
-        </br>
+return(
+    <>
         <ul>
-            <li>primeiro</li>
-            <li>segundo </li>
+            <li><Link href="/">HOME</Link></li>
+            <li><Link href="https://www.google.com">google</Link></li>
+            <li><Link href="https://vercel.com">Vercel</Link></li>
+            <li><Link href="/receitas">receitas</Link></li>
+            <li><Link href="">sobre</Link></li>
+            
+
         </ul>
-    </div>
+    </>
+)
+
 }
 
 export default MENU
